619a9a3728f0888ceec0cf12ed8c734f9338eb79
yournewgoodname <damislav@net.hr>
2022-10-25 20:40:46 +0200

    frontend skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+
 function App() {
-  const get = async () => {
-    const res = await fetch("http://localhost:8000");
-    console.log(res);
-  };
-  get();
-  return <div className="App">{}</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
